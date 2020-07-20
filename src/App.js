c7cf97ceb9d859e9dc86169442ce275d231e9697
心满 <LZL61219@163.com>
2020-07-20 22:46:02 +0800

    PrivacyPolicy

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
-import React from 'react';
+import React from 'react'
+import { BrowserRouter, Route, Link } from 'react-router-dom'
+import PrivacyPolicy from './views/privacyPolicy'
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <ul style={{ display: 'none' }}>
+          <li><Link to="/">首页</Link></li>
+          <li><Link to="/list/">列表</Link></li>
+        </ul>
+        <Route path="/" exact component={Index}/>
+        <Route path="/list/" component={List}/>
+      </BrowserRouter>
+    </div>
+  )
 }
 
-export default App;
+function Index() {
+  return <PrivacyPolicy />
+}
+function List() {
+  return <h2>List</h2>
+}
+
+export default App
---
+++ src/views/privacyPolicy.js
@@ -0,0 +1,50 @@
+import React from 'react'
+
+function PrivacyPolicy() {
+  return <>
+    <p>
+      本软件尊重并保护所有使用服务用户的个人隐私权。为了给您提供更准确、更有个性化的服务，本软件会按照本隐私权政策的规定使用和披露您的个人信息。但本软件将以高度的勤勉、审慎义务对待这些信息。除本隐私权政策另有规定外，在未征得您事先许可的情况下，本软件不会将这些信息对外披露或向第三方提供。本软件会不时更新本隐私权政策。您在同意本软件服务使用协议之时，即视为您已经同意本隐私权政策全部内容。本隐私权政策属于本软件服务使用协议不可分割的一部分。
+    </p>
+
+    <h3>1.适用范围</h3>
+    <p>a)在您使用本软件网络服务，本软件会使用的您的个人访问信息；</p>
+
+    <h3>2.信息的使用</h3>
+    <p>a)在获得您的数据之后，本软件会根据特点条件触发相应的软件功能，从而使您更好地与本软件进行交互。</p>
+
+    <h3>3.信息披露</h3>
+    <p>a)本软件不会将您的信息披露给不受信任的第三方。</p>
+    <p>b)根据法律的有关规定，或者行政或司法机构的要求，向第三方或者行政、司法机构披露；</p>
+    <p>c)如您出现违反您所在国家有关法律、法规或者相关规则的情况，需要向第三方披露；</p>
+
+    <h3>4.信息存储和交换</h3>
+    <p>本软件收集的有关您的信息和资料不会被上传到服务器上，这些信息和资料也不会被传送至您所在国家、地区或本软件收集信息和资料所在地的境外并在境外被访问、存储和展示。</p>
+
+    <h3>5.信息安全</h3>
+    <p>在使用本软件网络服务进行网络传输时，请您妥善保护自己的个人信息。如您发现自己的个人信息泄密，请您立即联络本软件客服，以便本软件采取相应措施。</p>
+
+    <p>
+      The Software respects and protects the personal privacy of all users who use the Services. In order to provide you with more accurate and personalized services, the Software will use and disclose your personal information in accordance with this Privacy Policy. However, the Software will treat this information with a high degree of diligence and care. Unless otherwise provided in this Privacy Policy, the Software will not disclose or provide this information to third parties without your prior permission. This Privacy Policy is updated from time to time. When you agree to the Use Agreement for the Use of the Software Services, you are deemed to have consented to the full contents of this Privacy Policy. This Privacy Policy is an integral part of the Software Services Use Agreement.
+    </p>
+
+    <h3>1. Scope of application</h3>
+    <p>a) In your use of the Software Network Services, the Software will use your personal access information;</p>
+
+    <h3>2. Use of information</h3>
+    <p>a) Once your data is obtained, the Software will trigger the appropriate software features according to the characteristics of the conditions, so that you can better interact with the Software.</p>
+
+    <h3>3. Information Disclosure</h3>
+    <p>a) The Software will not disclose your information to untrusted third parties.</p>
+    <p>b) Disclosure to third parties or administrative and judicial bodies in accordance with the relevant provisions of the law, or at the request of the administrative or judicial bodies;</p>
+    <p>c) if you violate the relevant laws, regulations or related rules of your country, you need to disclose it to a third party;</p>
+
+    <h3>4. Information storage and exchange</h3>
+    <p>Information and materials collected by the Software will not be uploaded to the server and will not be transmitted to or from outside your country, region or location where the Software collects and materials, and are accessed, stored and displayed abroad.</p>
+
+    <h3>5.Information Security</h3>
+    <p>When using the Software Network Services for network transmission, please protect your personal information properly. If you find your personal information compromised, please contact the software customer service immediately, so that the software to take appropriate action.</p>
+
+  </>
+}
+
+export default PrivacyPolicy
